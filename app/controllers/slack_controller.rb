f073e4c73ca1153eca6e248e2e2c74861c056dc0
Julian R Figueroa <julian.f@torre.co>
2018-05-03 18:53:44 -0500

    Slack Help command

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,2 +1,31 @@
+require 'http'
+
 class SlackController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :slash_command_params, only: [:help]
+  def help
+    response_url = params[:response_url]
+    # HTTP.post(response_url, json: { text: "Hello World! #{params[:user_name]}" })
+    render status: 200, body: nil
+    HTTP.post('https://hooks.slack.com/services/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', json: {
+      text: "Hello World! #{params[:user_name]}, Text: \"#{params[:text]}\""
+    })
+  end
+
+  private
+    def slash_command_params
+      params.permit(
+        :channel_id,
+        :channel_name,
+        :command,
+        :response_url,
+        :team_domain,
+        :team_id,
+        :text,
+        :token,
+        :trigger_id,
+        :user_id,
+        :user_name
+      )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope 'slack', controller: :slack do
+    post 'help', action: :help, as: :slack_help
+  end
 end

86331ee11ca0e76ee73c4c3d4cfa6406c0d370aa
Julian R Figueroa <julian.f@torre.co>
2018-05-04 00:13:58 -0500

    Added responding to messages and buttons

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,15 +1,94 @@
 require 'http'
 
 class SlackController < ApplicationController
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   before_action :slash_command_params, only: [:help]
+  before_action :interactive_params, only: [:interactive]
+
   def help
-    response_url = params[:response_url]
-    # HTTP.post(response_url, json: { text: "Hello World! #{params[:user_name]}" })
+    render status: 200, json: {
+        text: 'Climate challenge help. Here are the *slash commands* you can use.',
+        attachments: [
+          {
+            text: '*/ch-help* to get this message',
+            color: 'good'
+          },
+          {
+            text: '*/ch-test* to start a new test for yourself',
+            color: 'good'
+          },
+          {
+            text: '*/ch-results* to see the general results',
+            color: 'good'
+          },
+          {
+            text: 'Don\'t hesitate to ping us with new ideas',
+            footer: 'Made with :green_heart: by @luisa, @santiagoballen and @julian.f',
+            footer_icon: 'https://static1.squarespace.com/static/54d12b4de4b02f3989468d9c/t/59c9058146c3c452e8417bfb/1519144008724/imgtp-social.png?format=300w'
+          }
+        ]
+      }
+  end
+
+  def test
     render status: 200, body: nil
-    HTTP.post('https://hooks.slack.com/services/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', json: {
-      text: "Hello World! #{params[:user_name]}, Text: \"#{params[:text]}\""
-    })
+    response_url = params[:response_url]
+    HTTP.post(response_url,
+      json: {
+        "text": "Would you like to play a game?",
+        "attachments": [
+          {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+              {
+                "name": "game",
+                "text": "Chess",
+                "type": "button",
+                "value": "chess"
+              },
+              {
+                "name": "game",
+                "text": "Falken's Maze",
+                "type": "button",
+                "value": "maze"
+              },
+              {
+                "name": "game",
+                "text": "Thermonuclear War",
+                "style": "danger",
+                "type": "button",
+                "value": "war",
+                "confirm": {
+                  "title": "Are you sure?",
+                  "text": "Wouldn't you prefer a good game of chess?",
+                  "ok_text": "Yes",
+                  "dismiss_text": "No"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+  end
+
+  def interactive
+    render status: 200, plain: 'ok'
+    payload = JSON.parse(params[:payload])
+    response = HTTP.headers(
+      'Authorization': "Bearer #{ENV['SLACK_BOT_AUTH']}",
+      'Content-type': 'application/json'
+    ).post('https://slack.com/api/chat.postMessage',
+      json: {
+        text: 'Hola Mundo!',
+        channel: payload['channel']['id']
+      }
+    )
+    puts response
   end
 
   private
@@ -27,5 +106,9 @@ class SlackController < ApplicationController
         :user_id,
         :user_name
       )
+    end
+
+    def interactive_params
+      params.permit(:payload)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope 'slack', controller: :slack do
     post 'help', action: :help, as: :slack_help
+    post 'test', action: :test, as: :slack_test
+    post 'interactive', action: :interactive, as: :slack_interactive
+    post 'interactive_load', action: :interactive_load, as: :slack_interactive_load
   end
 end
